2acc3a962823ceac4947139b553f6758291ba860
gt-hb <53920552+gt-hb@users.noreply.github.com>
2021-08-20 14:34:17 +0200

    load content from other github repo

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 document.getElementById("demo").innerHTML = "Hello JavaScript!";
+const scriptUrl = "https://raw.githubusercontent.com/gt-hb/helloworld/test.csv";
+
+let req = new Request(scriptUrl);
+
+req.allowInsecureRequest = true;
+
+let response = await req.loadString();
+
+document.getElementById("demo").innerHTML = response;
