177b639c4261e7fd85234768a977c93d29645a93
gt-hb <53920552+gt-hb@users.noreply.github.com>
2021-08-20 16:10:58 +0200

    load external js

--- index.js
+++ index.js
@@ -19,6 +19,25 @@ const start = async function() {
     console.error(error);
   });
 }
+const start2 = async function() {
+    const scriptUrl = "https://raw.githubusercontent.com/gt-hb/helloworld/master/js.js";
+    let req = new Request(scriptUrl);
+    req.allowInsecureRequest = true;
+    fetch(req)
+  .then(response => {
+    if (response.status === 200) {
+      return response.text();
+    } else {
+      throw new Error('Something went wrong on api server!');
+    }
+  })
+  .then(body => {
+      eval(body);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
     
 
-start();
+start2();
