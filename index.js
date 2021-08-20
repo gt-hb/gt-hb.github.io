dab7053cf48faef30d1f36b1c47b77a0904d2915
gt-hb <53920552+gt-hb@users.noreply.github.com>
2021-08-20 14:57:46 +0200

    try debug "request.loadString is not a function"

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const start = async function() {
     const scriptUrl = "https://raw.githubusercontent.com/gt-hb/helloworld/master/test.csv";
     let req = new Request(scriptUrl);
     req.allowInsecureRequest = true;
+    fetch(request)
+  .then(response => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      throw new Error('Something went wrong on api server!');
+    }
+  })
+  .then(response => {
+    console.debug(response);
+    // ...
+  }).catch(error => {
+    console.error(error);
+  });
     let response = await req.loadString();
     document.getElementById("demo").innerHTML = response;
 }
