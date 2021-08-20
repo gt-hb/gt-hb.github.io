c3316fac77fd0183f13e2f421618cb075ae58921
gt-hb <53920552+gt-hb@users.noreply.github.com>
2021-08-20 18:51:09 +0200

    retry foreign URI

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 async function start(){
-  const { foo } = await import("./mjs.mjs");
-  //const { foo } = await import("https://raw.githubusercontent.com/gt-hb/helloworld/master/mjs.mjs");
+  //const { foo } = await import("./mjs.mjs");
+  const { foo } = await import("https://raw.githubusercontent.com/gt-hb/helloworld/master/mjs.mjs");
   foo();
 }
 
